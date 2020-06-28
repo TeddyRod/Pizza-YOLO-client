30d57741c6939c7b8a4956cabade84037ca54112
Teddy ROGERS <teddy.rogers.pro@outlook.com>
2020-06-28 23:48:58 +0200

    Update Ingredients.jsx

--- src/pages/Ingredients.jsx
+++ src/pages/Ingredients.jsx
@@ -4,7 +4,7 @@ import IngredientsCmpt from "../components/IngredientsCmpt";
 const Home = (props) => {
   return (
     <div>
-      <h1>Ingredients Page <span role="img">✌️</span></h1>
+      <h1>Ingredients Page </h1>
         <IngredientsCmpt />
     </div>
   );
